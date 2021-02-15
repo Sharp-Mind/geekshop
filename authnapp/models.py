28948f1a9865fd14bdc69d777135ec8729e8ebaa
Sharp_Mind <redlance988@gmail.com>
2021-02-15 13:15:58 +0300

    Lesson 4. ShopUser model restored (field "null=True" deleted)

--- authnapp/models.py
+++ authnapp/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to="users_avatars", blank=True)
-    age = models.PositiveIntegerField(verbose_name="возраст", null=True)
+    age = models.PositiveIntegerField(verbose_name="возраст")
