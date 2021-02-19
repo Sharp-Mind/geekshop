0eb9b26a2b2c0821c3e1e08c6301ce57347c4544
Sharp_Mind <redlance988@gmail.com>
2021-02-19 18:48:40 +0300

    Lesson 6. Basket read and delete. (code formatted by 'isort' and 'black' utils)

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
 from basketapp.models import Basket
@@ -5,12 +6,13 @@ from mainapp.models import Product
 
 
 def basket(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+    title = "корзина"
+    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    return render(request, "basketapp/basket.html", content)
 
 
-def basket_add(request, pk, total_sum=0):
+def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
 
@@ -18,13 +20,12 @@ def basket_add(request, pk, total_sum=0):
         basket = Basket(user=request.user, product=product)
 
     basket.quantity += 1
-    basket.price = product.price
     basket.save()
 
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def basket_remove(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
