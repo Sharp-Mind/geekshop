b4c584e689b4704a32fb3ebfd1742510e589fc72
Sharp-Mind <64483275+Sharp-Mind@users.noreply.github.com>
2021-03-08 14:15:37 +0300

    Lesson 9 (#9)
    * Lesson 8+. Fully working users paginator on admin page
    
    * lesson_9. URLs as regexp
    
    * lesson_9. Send email to file.
    
    * lesson_9. Context processor.

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,32 +1,17 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import re_path
 
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path("", mainapp.main, name="main"),
-    path("products/", include("mainapp.urls", namespace="products")),
-    path("contact/", mainapp.contact, name="contact"),
-    path("auth/", include("authnapp.urls", namespace="auth")),
-    path("basket/", include("basketapp.urls", namespace="basket")),
-    path("admin/", include("adminapp.urls", namespace="admin")),
+    re_path(r"^$", mainapp.main, name="main"),
+    re_path(r"^products/", include("mainapp.urls", namespace="products")),
+    re_path(r"^contact/", mainapp.contact, name="contact"),
+    re_path(r"^auth/", include("authnapp.urls", namespace="auth")),
+    re_path(r"^basket/", include("basketapp.urls", namespace="basket")),
+    re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
 ]
 
 
